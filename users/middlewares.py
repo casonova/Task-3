23573d1a61052c74458bd49986a837ef311bb790
Rehan Habib <rehan.habib777@gmail.com>
2023-11-16 21:01:39 +0500

    Adjust Custom Middleware

--- users/middlewares.py
+++ users/middlewares.py
@@ -1,12 +1,12 @@
 from django.http import HttpResponse
 from django.utils.deprecation import MiddlewareMixin
 
+class CustomAuthenticationMiddleware(MiddlewareMixin):
+    def __init__(self, get_response):
+        self.get_response = get_response
 
-class AuthenticationMIddleware(MiddlewareMixin):
-    def process_view(self, request, view_func, *view_args, **view_kwargs):
-        user = request.user
-        print(user)
-        if user.is_authenticated:
-            return HttpResponse(status=200)
-        else:
-            return HttpResponse('Unauthorised', status=401)
+    def __call__(self, request):
+        if not hasattr(request, 'session') or not request.user.is_authenticated:
+            return HttpResponse("Not Authorised", status=401)  
+        response = self.get_response(request)
+        return response
