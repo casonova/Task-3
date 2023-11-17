545f102ac157d1f5ee0eae038de60c67efb7e9f1
Rehan Habib <rehan.habib777@gmail.com>
2023-11-17 12:10:05 +0500

    Format Middleware File

--- users/middlewares.py
+++ users/middlewares.py
@@ -1,12 +1,13 @@
 from django.http import HttpResponse
 from django.utils.deprecation import MiddlewareMixin
 
+
 class CustomAuthenticationMiddleware(MiddlewareMixin):
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        if not hasattr(request, 'session') or not request.user.is_authenticated:
+        if   not request.user.is_authenticated:
             return HttpResponse("Not Authorised", status=401)  
         response = self.get_response(request)
         return response
