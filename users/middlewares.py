160f30379b581cade9abb92787cd15078e508688
Rehan Habib <rehan.habib777@gmail.com>
2023-11-20 11:18:44 +0500

    Update MIddleware with login authentication

--- users/middlewares.py
+++ users/middlewares.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.utils.deprecation import MiddlewareMixin
+from users.models import User
 
 
 class CustomAuthenticationMiddleware(MiddlewareMixin):
@@ -7,7 +8,24 @@ class CustomAuthenticationMiddleware(MiddlewareMixin):
         self.get_response = get_response
 
     def __call__(self, request):
-        if   not request.user.is_authenticated:
-            return HttpResponse("Not Authorised", status=401)  
+        if not request.user.is_authenticated:
+            if request.method == "POST":
+                email = request.POST.get('username')
+                password = request.POST.get('password')
+                user = self.authenticate_user(email, password)
+                if user is not None:
+                    request.user = user 
+                else:
+                    return HttpResponse("Invalid Credentials")
         response = self.get_response(request)
         return response
+    
+    def authenticate_user(self, email, password):
+        try:
+            user = User.objects.get(email=email)
+            if user.check_password(password):
+                return user     
+            else:
+                return None 
+        except User.DoesNotExist:
+            return None
